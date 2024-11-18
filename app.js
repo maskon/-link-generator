074530fc7c0612199c0425dce94d0013c83e7ff2
Максим Блинов <126991331+maskon@users.noreply.github.com>
2024-11-18 19:15:46 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -3,13 +3,17 @@ const btn = document.getElementById('btn')
 const copy = document.getElementById('btn-copy')
 const result = document.getElementById('result')
 const quantity = document.getElementById('quantity')
-const completed = document.getElementById('completed')
 
 let dataString = ''
 
-input.addEventListener('input', () => {dataString = input.value})
+input.addEventListener('input', () => {
+    input.value ? btn.disabled = false : btn.disabled = true
+    dataString = input.value
+})
 
 btn.addEventListener('click', () => {
+    btn.disabled = true
+    copy.disabled = false
     const array = dataString.split(', ').map(item => item.trim())
     quantity.textContent = `Количество обработанных ссылок: ${array.length}`
 
@@ -25,8 +29,7 @@ btn.addEventListener('click', () => {
 copy.addEventListener('click', () => {
     navigator.clipboard.writeText(result.textContent).then(() => {
         console.log('Текст успешно скопирован в буфер обмена')
-        completed.style.opacity = 1
-        showBlock()
+        copy.disabled = true
         input.value = ''
         input.focus()
         result.textContent = ''
@@ -34,7 +37,3 @@ copy.addEventListener('click', () => {
       }, err => { console.error('Произошла ошибка при копировании текста: ', err) }
     )
 })
-
-function showBlock() {
-    setTimeout( () => completed.style.opacity = 0, 700)
-}

e1594afe19037306b6b8f5add17c4d9e2245717c
Максим Блинов <126991331+maskon@users.noreply.github.com>
2024-11-18 15:31:22 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -2,18 +2,21 @@ const input = document.getElementById('input')
 const btn = document.getElementById('btn')
 const copy = document.getElementById('btn-copy')
 const result = document.getElementById('result')
+const quantity = document.getElementById('quantity')
+const completed = document.getElementById('completed')
 
-let dataString
+let dataString = ''
 
 input.addEventListener('input', () => {dataString = input.value})
 
 btn.addEventListener('click', () => {
     const array = dataString.split(', ').map(item => item.trim())
+    quantity.textContent = `Количество обработанных ссылок: ${array.length}`
 
     const formattedArray = array.map(item => {
         return item
             .replace(/file\/d\//g, 'uc?export=download&id=')
-            .replace(/\/view\?usp=drive_link/, '')
+            .replace(/\/view\?usp=drive_link|\/view\?usp=sharing/, '')
     })
 
     result.textContent = formattedArray.join(', ')
@@ -22,9 +25,16 @@ btn.addEventListener('click', () => {
 copy.addEventListener('click', () => {
     navigator.clipboard.writeText(result.textContent).then(() => {
         console.log('Текст успешно скопирован в буфер обмена')
+        completed.style.opacity = 1
+        showBlock()
         input.value = ''
         input.focus()
         result.textContent = ''
+        quantity.textContent = 'Вставьте ссылки в поле ниже:'
       }, err => { console.error('Произошла ошибка при копировании текста: ', err) }
     )
 })
+
+function showBlock() {
+    setTimeout( () => completed.style.opacity = 0, 700)
+}
